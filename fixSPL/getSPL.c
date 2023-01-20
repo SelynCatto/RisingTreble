812b0e8bff7f4f40ca3c168c7c220c290f7842fa
Pierre-Hugues Husson <phh@phh.me>
2023-01-20 06:01:26 -0500

    Upgrade getSPL to support reading SPL/Android version from AVB footer (ugly code, but should be safe,and proved to work at least once)

--- fixSPL/getSPL.c
+++ fixSPL/getSPL.c
@@ -1,8 +1,10 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,14 +27,42 @@ int main(int argc, char **argv) {
 	int y = 2000 + (spl >> 4);
 	int m = spl & 0xf;
 
-	fprintf(stderr, "Android: %d.%d.%d\n", a, b, c);
-	fprintf(stderr, "SPL: %d-%d-01\n", y, m);
 
-	if(strcmp(argv[2], "android") == 0) {
-		printf("%d.%d.%d", a, b, c);
-	} else if(strcmp(argv[2], "spl") == 0) {
-		printf("%04d-%02d-%02d", y, m, 1);
-	}
+    if (spl != 0) {
+        fprintf(stderr, "Android: %d.%d.%d\n", a, b, c);
+        fprintf(stderr, "SPL: %d-%d-01\n", y, m);
+        if(strcmp(argv[2], "android") == 0) {
+            printf("%d.%d.%d", a, b, c);
+        } else if(strcmp(argv[2], "spl") == 0) {
+            printf("%04d-%02d-%02d", y, m, 1);
+        }
+        return 0;
+    }
+
+	off_t size = lseek(fd, 0, SEEK_END);
+	lseek(fd, 0, SEEK_SET);
+    char *buf = malloc(size);
+    read(fd, buf, size);
+
+#define PROP_OS_VERSION "com.android.build.boot.os_version"
+#define PROP_SPL "com.android.build.boot.security_patch"
+    char* p = memmem(buf, size, PROP_OS_VERSION, sizeof(PROP_OS_VERSION));
+    if(p != NULL) {
+          p += sizeof(PROP_OS_VERSION);
+          fprintf(stderr, "Android: %s\n", p);
+          if(strcmp(argv[2], "android") == 0) {
+              printf("%s.0.0", p);
+          }
+    }
+
+    p = memmem(buf, size, PROP_SPL, sizeof(PROP_SPL));
+    if(p != NULL) {
+          p += sizeof(PROP_SPL);
+          fprintf(stderr, "SPL: %s\n", p);
+          if(strcmp(argv[2], "spl") == 0) {
+              printf("%s", p);
+          }
+    }
 
 	return 0;
 }
